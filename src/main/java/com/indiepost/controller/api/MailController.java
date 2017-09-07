d299e2b033d19378574c6e1fee12500c03c24ed0
Jake Lee <bwv1050@gmail.com>
2017-09-08 01:21:24 +0900

    Version 1.6.2: Fix mail api uri mapping error.

--- src/main/java/com/indiepost/controller/api/MailController.java
+++ src/main/java/com/indiepost/controller/api/MailController.java
@@ -6,6 +6,7 @@ import com.indiepost.service.MailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
@@ -13,7 +14,8 @@ import javax.validation.Valid;
 /**
  * Created by jake on 8/31/17.
  */
-@RestController("/api/mail")
+@RestController
+@RequestMapping("/api/mail")
 public class MailController {
 
     private final MailService mailService;
